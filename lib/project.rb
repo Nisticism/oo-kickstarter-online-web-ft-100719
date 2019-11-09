43cf45b48bc8dd957db6f2dee50c490350541a8a
Hans Foster <fosterhans@gmail.com>
2019-11-09 02:48:48 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project 
+  
+  attr_reader :backers, :title
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.
+    
+  end
+  
+  
+end
